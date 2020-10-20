b636abe8e08bafabf60e1133af8b0dc83f7ac91b
kokubun-sato <hsato@daishodai.ac.jp>
2020-10-20 12:38:42 +0900

    change 10-20

---
+++ section2-q1-black-circle/sketch.js
@@ -0,0 +1,9 @@
+// 小手調べ
+function setup(){
+  noFill();
+  for(let i = 0; i < 10; i++){
+    let s = i * 10;
+    ellipse(50,50,s,s);
+    // BLANK[1]
+  }
+}
---
+++ section2-q1-color-circle/sketch.js
@@ -0,0 +1,15 @@
+// 小手調べ
+function setup(){
+  noFill();
+  for(let i = 0; i < 10; i++){
+    let s = i * 10;
+    ellipse(50,50,s,s);
+    if(i <= 5){
+      stroke(255.,0,0);
+  }
+    else {
+      stroke(0,0,255);
+    }
+    // BLANK[1]
+  }
+}
